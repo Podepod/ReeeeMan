3c3ab547c4301b53b3bc765f2c4026fe82673b63
Podepod <lode.gilis@gmail.com>
2020-12-30 17:04:11 +0100

    autonav test

--- views/router.js
+++ views/router.js
@@ -45,65 +45,72 @@ router.get("/customText", laadCustomText);
 // Index pagina
 function laadIndex(req, res)
 {
-    pages = updateData("pages.json");
+    nav = updateData("pages.json");
 
-    res.render("index", {page: "Home", "navId": "indexNav", "data": pages});
+    res.render("index", {page: "Home", "navId": "indexNav", "nav": nav});
 }
 
 // Instellingen pagina
 function laadInstellingen(req, res)
 {
     instellingen = updateData("botSettings.json");
+    nav = updateData("pages.json");
 
-    res.render("instellingen", {page: "Instellingen", "navId": "instellingenNav", "instellingen": instellingen});
+    res.render("instellingen", {page: "Instellingen", "navId": "instellingenNav", "nav": nav, "instellingen": instellingen});
 }
 
 // Permission Climbing Pagina
 function laadPermissionClimbing(req, res)
 {
     config = updateData("permissionClimbing.json");
+    nav = updateData("pages.json");
 
-    res.render("permissionClimbing", {page: "Permission Climbing", "navId": "permissionClimbingNav", "config": config});
+    res.render("permissionClimbing", {page: "Permission Climbing", "navId": "permissionClimbingNav", "nav": nav, "config": config});
 }
 
 // Regex Search Words Pagina
 function laadRegexSearchWord(req, res)
 {
     searchWords = updateData("regexSearchWords.json");
+    nav = updateData("pages.json");
 
-    res.render("regexSearchWords", {page: "Regex Search Words", "navId": "regexSearchWordsNav", "data": searchWords});
+    res.render("regexSearchWords", {page: "Regex Search Words", "navId": "regexSearchWordsNav", "nav": nav, "data": searchWords});
 }
 
 // Regex Reactions Pagina
 function laadRegexReactions(req, res)
 {
     searchWords = updateData("regexReactions.json");
+    nav = updateData("pages.json");
 
-    res.render("regexReactions", {page: "Regex Reactions", "navId": "regexReactionsNav", "data": searchWords});
+    res.render("regexReactions", {page: "Regex Reactions", "navId": "regexReactionsNav", "nav": nav, "data": searchWords});
 }
 
 // Regex Bans Pagina
 function laadRegexBans(req, res)
 {
     searchWords = updateData("regexBans.json");
+    nav = updateData("pages.json");
 
-    res.render("regexBans", {page: "Regex Bans", "navId": "regexBansNav", "data": searchWords});
+    res.render("regexBans", {page: "Regex Bans", "navId": "regexBansNav", "nav": nav, "data": searchWords});
 }
 
 // DM Pagina
 function laadDMs(req, res)
 {
     DMlist = updateData("DMs.json");
+    nav = updateData("pages.json");
 
-    res.render("DMs", {page: "DMs", "navId": "DMNav", "data": DMlist});
+    res.render("DMs", {page: "DMs", "navId": "DMNav", "nav": nav, "data": DMlist});
 }
 
 // Custom Text Pagina
 function laadCustomText(req, res)
 {
     text = updateData("customText.json");
+    nav = updateData("pages.json");
 
-    res.render("customText.ejs", {page: "Custom Text", "navId": "customTextNav", "customText": text.text});
+    res.render("customText.ejs", {page: "Custom Text", "navId": "customTextNav", "nav": nav, "customText": text.text});
 }
 
 module.exports = router;
