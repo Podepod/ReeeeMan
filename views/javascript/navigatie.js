27dd6f639ffadb62ee3970e51612f97d758bbd85
Podepod <lode.gilis@gmail.com>
2021-01-04 21:17:15 +0100

    1.0.9 console log weg

--- views/javascript/navigatie.js
+++ views/javascript/navigatie.js
@@ -1,7 +1,6 @@
 window.onload = function(){
     var id = document.getElementsByName('active-nav')[0].getAttribute('content');
     if(document.getElementById(id).className == 'dropdown-item'){
-      console.log("dropdown");
       parentId = document.getElementById(id).parentElement.getAttribute('aria-labelledby')
       document.getElementById(parentId).className += ' active';
       document.getElementById(parentId).content += ' <span class="sr-only">(current)</span>';
@@ -13,7 +12,6 @@ window.onload = function(){
   function navigatie(){
     var id = document.getElementsByName('active-nav')[0].getAttribute('content');
     if(document.getElementById(id).className == 'dropdown-item'){
-      console.log("dropdown");
       parentId = document.getElementById(id).parentElement.getAttribute('aria-labelledby')
       document.getElementById(parentId).className += ' active';
       document.getElementById(parentId).content += ' <span class="sr-only">(current)</span>';
