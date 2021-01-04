3161007375b76570bc608818b3b976e3c97e493f
Podepod <lode.gilis@gmail.com>
2021-01-04 21:06:03 +0100

    1.0.9 javascript test

--- views/javascript/navigatie.js
+++ views/javascript/navigatie.js
@@ -1,11 +1,23 @@
 window.onload = function(){
     var id = document.getElementsByName('active-nav')[0].getAttribute('content');
+    if(document.getElementById(id).className == 'dropdown-item'){
+      console.log("dropdown");
+      parentId = document.getElementById(id).parentElement.getAttribute('aria-labelledby')
+      document.getElementById(parentId).className += ' active';
+      document.getElementById(parentId).content += ' <span class="sr-only">(current)</span>';
+    }
     document.getElementById(id).className += ' active';
     document.getElementById(id).content += ' <span class="sr-only">(current)</span>';
   }
   
   function navigatie(){
     var id = document.getElementsByName('active-nav')[0].getAttribute('content');
+    if(document.getElementById(id).className == 'dropdown-item'){
+      console.log("dropdown");
+      parentId = document.getElementById(id).parentElement.getAttribute('aria-labelledby')
+      document.getElementById(parentId).className += ' active';
+      document.getElementById(parentId).content += ' <span class="sr-only">(current)</span>';
+    }    
     document.getElementById(id).className += ' active';
     document.getElementById(id).content += ' <span class="sr-only">(current)</span>';
   }
