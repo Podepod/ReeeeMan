c802c259b79208e715960354c8da312897c82d92
Podepod <lode.gilis@gmail.com>
2021-01-07 13:33:41 +0100

    icon fix?

--- cogs/Events.py
+++ cogs/Events.py
@@ -69,7 +69,7 @@ class Events(commands.Cog):
 
         for guild in self.bot.guilds:
             embed = discord.Embed(title=f"{guild.name}", description=f"Created at: {guild.created_at}\n Region: {guild.region}\n Owner: {guild.owner}", timestamp=datetime.datetime.utcnow(), color=discord.Color.red())
-            embed.set_image(guild.icon_url)
+            embed.set_image(string(guild.icon_url))
             for channel in guild.text_channels:
                 embed.add_field(
                     name=channel.name,
