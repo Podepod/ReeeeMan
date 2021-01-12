e6ea6128715fc95926e25e845be9ca1d10252167
Podepod <lode.gilis@gmail.com>
2021-01-12 19:15:39 +0100

    Music bot test

---
+++ cogs/Music.py
@@ -0,0 +1,74 @@
+import discord
+from discord.ext import tasks, commands
+import datetime
+import asyncio
+import re
+import random
+import apiRequests as api
+import wavelink
+
+config = api.readConfig()
+
+bot = commands.Bot(command_prefix=config["basic"]["prefix"], description=config["basic"]["description"])
+
+# Music COG
+class Music(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        self.config = api.readConfig()
+
+        self.configLoop.start()
+
+    def cog_unload(self):
+        self.configLoop.cancel()
+
+    async def start_nodes(self):
+        await self.bot.wait_until_ready()
+
+        # Initiate our nodes. For this example we will use one server.
+        # Region should be a discord.py guild.region e.g sydney or us_central (Though this is not technically required)
+        await self.bot.wavelink.initiate_node(host='127.0.0.1',
+                                              port=2333,
+                                              rest_uri='http://127.0.0.1:2333',
+                                              password='youshallnotpass',
+                                              identifier='TEST',
+                                              region='us_central')
+
+    @bot.command(hidden = True, name="connect")
+    async def connect_(self, ctx, *, channel: discord.VoiceChannel=None):
+        if not channel:
+            try:
+                channel = ctx.author.voice.channel
+            except AttributeError:
+                raise discord.DiscordException('No channel to join. Please either specify a valid channel or join one.')
+
+        player = self.bot.wavelink.get_player(ctx.guild.id)
+        await ctx.send(f'Connecting to **`{channel.name}`**')
+        await player.connect(channel.id)
+
+    @bot.command(hidden = True, name="connect")
+    async def play(self, ctx, *, query: str):
+        tracks = await self.bot.wavelink.get_tracks(f'ytsearch:{query}')
+
+        if not tracks:
+            return await ctx.send('Could not find any songs with that query.')
+
+        player = self.bot.wavelink.get_player(ctx.guild.id)
+        if not player.is_connected:
+            await ctx.invoke(self.connect_)
+
+        await ctx.send(f'Added {str(tracks[0])} to the queue.')
+        await player.play(tracks[0])
+
+    @tasks.loop(seconds=5.0)
+    async def configLoop(self):
+        self.config = api.readConfig()
+
+    @configLoop.before_loop
+    async def beforeConfigLoop(self):
+        await self.bot.wait_until_ready()
+
+
+def setup(bot):
+    bot.add_cog(Music(bot))
