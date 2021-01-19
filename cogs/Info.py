24e61c0ce31c5d2c2a095144b9e8df587b736bd4
Podepod <lode.gilis@gmail.com>
2021-01-19 23:38:09 +0100

    1.4.6 fix

--- cogs/Info.py
+++ cogs/Info.py
@@ -133,17 +133,17 @@ class Info(commands.Cog):
             description=f"A little info about {ctx.guild.name}'s {ctx.guild.member_count} members",
             timestamp=datetime.datetime.utcnow(), 
             color=discord.Color.red()
-        )
-        embed.set_thumbnail(url=ctx.guild.icon_url)
-
-        for gMember in ctx.guild.members:
-            embed.add_field(
-                name=gMember.name,
-                value=f"Nickname: {gMember.nick}\nIs bot? {gMember.bot}\nJoined at: {gMember.joined_at}",
-                inline=False
             )
+            embed.set_thumbnail(url=ctx.guild.icon_url)
 
-        await ctx.send(embed=embed)
+            for gMember in ctx.guild.members:
+                embed.add_field(
+                    name=gMember.name,
+                    value=f"Nickname: {gMember.nick}\nIs bot? {gMember.bot}\nJoined at: {gMember.joined_at}",
+                    inline=False
+                )
+
+            await ctx.send(embed=embed)
 
     # PING
     # command
